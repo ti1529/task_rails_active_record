83427d6c400e17db6f786901dcb1a4385e094f69
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-02-09 12:34:40 +0900

    initial commit

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,13 +2,13 @@ class ExercisesController < ApplicationController
   def exercise1
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
-    @foods = Food
+    @foods = Food.left_outer_joins(:order_foods).where(order_foods: {id: [nil]})
   end
 
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-    @shops = Shop
+    @shops = Shop.jo
   end
 
   def exercise3 
